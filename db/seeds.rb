f31d7c9774baebeeabf7b7addd42cf4a3cc0ee95
encalien <katarina.mursec@gmail.com>
2019-01-15 16:51:29 +0100

    created Users and Messages table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'Luke', password: "password")
+User.create(username: 'Anna', password: "password")
+User.create(username: 'John', password: "password")
+User.create(username: 'Mike', password: "password")
+User.create(username: 'Alex', password: "password")
