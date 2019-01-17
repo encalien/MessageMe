eec1da912534c41e660c07ff79f2cf19740c6ab7
encalien <katarina.mursec@gmail.com>
2019-01-17 22:01:01 +0100

    added authentication system

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in!"
+      redirect_to root_path
+    else
+      flash.now[:error] = "There was an error with your login. Please try again!"
+      render "new"
+    end
   end
 
   def destroy
-    
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out!"
+    redirect_to login_path
   end
 end
