4b6692f83fdc77cb99321d623de01bbc7861376d
encalien <katarina.mursec@gmail.com>
2019-01-18 01:07:30 +0100

    finishing touches for chatbox

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,14 +19,23 @@
 scroll_bottom = function() {
 	if ($('#messages').length >0) {
 		$('#messages').scrollTop($('#messages')[0].scrollHeight);
-	}
-}
+	};
+};
+
+clear_message_field = function() {
+	$('#message_body').on('keyup', function(e) {
+		if (e.keyCode == 13) {
+			e.target.value = "";
+		};
+	});
+};
 
 $(document).on('turbolinks:load', function() {
 	$('.ui.dropdown').dropdown();
 	$('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+  clear_message_field();
   scroll_bottom();
 })
 
